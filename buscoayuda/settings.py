e6a1fc95f80caf69a4446fe55eca6ccebb2214e6
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2016-10-05 10:34:21 -0500

    Paso 1 - Test titulo (Verde)

--- buscoayuda/settings.py
+++ buscoayuda/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'buscoayuda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'polls/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
